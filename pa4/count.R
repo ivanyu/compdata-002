d967243f7486ab03aa936855962d89598f31c97e
ivanyu <ivanhoe12@gmail.com>
2013-01-29 20:00:00 +0400

    Comments from instruction added

--- pa4/count.R
+++ pa4/count.R
@@ -1,4 +1,13 @@
 count <- function(cause = NULL) {
+    ## Check that "cause" is non-NULL; else throw error
+    ## Check that specific "cause" is allowed; else throw error
+    
+    ## Read "homicides.txt" data file
+    
+    ## Extract causes of death
+    
+    ## Return integer containing count of homicides for that cause
+
     if (is.null(cause)) {
         stop("cause can't be NULL")
     }
